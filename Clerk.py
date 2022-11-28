0e245f79461644bfd915782c3e31cd6733f9e7be
Josh Faigan <djjoker13@gmail.com>
2022-11-28 18:34:14 -0500

    added clerk

--- Clerk.py
+++ Clerk.py
@@ -1,1 +1,27 @@
+from Person import Person
+
+class Clerk(Person):
+    def __init__(self, name, weekend_shift = False):
+        super().__init__(name)
+        self.amount_owed = 0
+        self.weekend_shift = True
+
+    def greet(self):
+        print(f"Hi I'm {self.name}. Are you here for a car wash?")
+    
+    def quit(self):
+        print(f"Looks like my shift is over! Time to go home.")
+
+    def what_shift(self):
+        if self.weekend_shift == True:
+            print(f"Yeah it's my turn to work the weekend...")
+        else:
+            print(f"Yay I have the weekend off!")
+    
+if __name__ == "__main__":
+    test_clerk = Clerk("Darius")
+    test_clerk.greet()
+    test_clerk.what_shift()
+    test_clerk.hand.append("Car Keys")
+    print(test_clerk.hand)
 
