be94bc7ad4333fdd05bae145c4693ceb925aa5d4
Josh Faigan <djjoker13@gmail.com>
2022-11-28 18:07:14 -0500

    added person

--- Person.py
+++ Person.py
@@ -1,1 +1,22 @@
+import random
 
+class Person(object):
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+    
+    def greet(self):
+        print(f"Hi, my name is {self.name}.")
+    
+    def quit(self):
+        print(f"I'm going home.")
+
+
+
+if __name__ == "__main__":
+    test_person = Person('Nick')
+    test_person_1 = Person('Lisa')
+    
+    print(test_person.name)
+    test_person_1.greet()
+    test_person.quit()
